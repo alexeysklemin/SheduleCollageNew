29458cea060d8afb461b47f343fd88d8a01b0bb8
a_skl <a_skl@LAPTOP-ALEX>
2022-03-27 08:10:52 +0400

    disined 2 forms and dataGridView object

--- SheduleCollageNew/FormDb.h
+++ SheduleCollageNew/FormDb.h
@@ -35,6 +35,8 @@ namespace SheduleCollageNew {
 			}
 		}
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ FirstColumn;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ SrcondColumn;
 	protected:
 
 	private:
@@ -51,17 +53,38 @@ namespace SheduleCollageNew {
 		void InitializeComponent(void)
 		{
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->FirstColumn = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->SrcondColumn = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(12, 27);
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) {
+				this->FirstColumn,
+					this->SrcondColumn
+			});
+			this->dataGridView1->Location = System::Drawing::Point(0, 32);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
-			this->dataGridView1->Size = System::Drawing::Size(240, 150);
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(270, 150);
 			this->dataGridView1->TabIndex = 0;
+			// 
+			// FirstColumn
+			// 
+			this->FirstColumn->HeaderText = L"First Column";
+			this->FirstColumn->MinimumWidth = 6;
+			this->FirstColumn->Name = L"FirstColumn";
+			this->FirstColumn->Width = 125;
+			// 
+			// SrcondColumn
+			// 
+			this->SrcondColumn->HeaderText = L"Srcond Column";
+			this->SrcondColumn->MinimumWidth = 6;
+			this->SrcondColumn->Name = L"SrcondColumn";
+			this->SrcondColumn->Width = 125;
 			// 
 			// FormDb
 			// 
